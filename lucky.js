3fd5412ee8e091536b7825238098fb76b6f57511
Luckyhudda <mr.luckyhudda@gmail.com>
2023-04-09 21:19:37 +0530

    Add user Detailes

---
+++ lucky.js
@@ -0,0 +1,12 @@
+// Git hub Project session with class 
+
+const userDetails = {
+    name: 'Lucky Hudda',
+    age: 22,
+    job: "Developer",
+    status: true,
+    frieds: ['Rahul', 'Malik', 'Meghana', 'sanjiv', 'Ankit', 'Deepali', 'Parishhh']
+}
+
+console.log(userDetails.name);
+
